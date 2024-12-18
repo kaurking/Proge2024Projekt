ccf956dcd1498e14a6e6a5ef13d7d274a726824a
kaurking <kaur.kingsepp13@gmail.com>
2024-12-18 12:28:08 +0200

    Lisasin Prisma scraperi seleniumiga. Eraldi programmina ka, võib selle kustutada pmst.

--- app.py
+++ app.py
@@ -38,9 +38,12 @@ def selver(sisend):
     # Eemaldab mingisugused kahtlased non-essential errorid seotud mingi USB jamaga (võib ära võtta, siis kood töötab, aga mingid sõnumid tulevad)
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+    # driver setup
     service = Service('chromedriver.exe')
     driver = webdriver.Chrome(service=service, options=options)
 
+    # Veebileht URL
     urllib.parse.quote(sisend)
     url = 'https://www.selver.ee/search?q=' + sisend
 
@@ -74,6 +77,59 @@ def selver(sisend):
             # Leiab Tootehinna kaardist
             hinnaelement = el.find_element(By.CLASS_NAME, 'ProductPrice')
             tootehind = hinnaelement.text.split("\n")[0]
+
+            # Paneb listi
+            hinnad.append(tootehind)
+
+    finally:
+        driver.quit()
+
+    return pd.DataFrame({'Toode': tooted[:max_read], 'Hind': hinnad[:max_read]})
+
+def prisma(sisend):
+
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+    # driver setup
+    service = Service('chromedriver.exe')
+    driver = webdriver.Chrome(service=service, options=options)
+    
+    # veebileht URL
+    urllib.parse.quote(sisend)
+    url = 'https://www.prismamarket.ee/' + 'products/search/' + sisend
+
+    driver.get(url)
+
+    # listid toodete jaoks
+    tooted = []
+    hinnad = []
+
+    try:
+        for i in range(1):  # scrollib lihtsalt faili lõppu?
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)  # ootab, et laadida contenti
+        
+        # Prisma lehel on kõik tooded "Kaartidena". 
+        tootekaart = driver.find_elements(By.CLASS_NAME, "js-shelf-item")
+
+        for el in tootekaart:
+            # Leiab Tootenime kaardist
+            nimeelement = el.find_element(By.CLASS_NAME, 'name')
+            tootenimi = nimeelement.text.strip()
+ 
+            # Paneb listi
+            tooted.append(tootenimi)
+            
+            # Leiab Tootehinna esimese osa kaardist
+            hinnaelement = el.find_element(By.CLASS_NAME, 'whole-number ')
+            tootehind1 = hinnaelement.text.strip()
+
+            # Leiab Tootehinna teise osa kaardist
+            hinnaelement = el.find_element(By.CLASS_NAME, 'decimal')
+            tootehind2 = hinnaelement.text.strip()
+
+            tootehind = f"{tootehind1}.{tootehind2}"
 
             # Paneb listi
             hinnad.append(tootehind)
